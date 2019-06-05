2dc743ea465a6f4e070d33e1708aca026f026470
Furkan <furkansertc@gmail.com>
2019-06-05 23:23:12 +0300

    V1

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnStart.setOnClickListener {
+            val intent = Intent(this,PauseMenu::class.java)
+
+            startActivity(intent)
+        }
+    }
+
+
+
+}
